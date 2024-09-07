0177598cf7e37f884f64acf69212732c1fdf0a8b
Dave Pagurek <davepagurek@gmail.com>
2024-09-07 13:59:55 -0400

    Update p5 script path

--- src/globals/globals.ts
+++ src/globals/globals.ts
@@ -15,7 +15,7 @@ export const eventsPerPage = 12 as const;
 
 // export const cdnLibraryUrl =
   // `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${p5Version}/p5.min.js` as const;
-export const cdnLibraryUrl = `/p5.min.js` as const;
+export const cdnLibraryUrl = `/p5.js-website/p5.min.js` as const;
 export const fullDownloadUrl =
   `https://github.com/processing/p5.js/releases/download/v${p5Version}/p5.zip` as const;
 export const libraryDownloadUrl =
